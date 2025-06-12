6b7c2ab69153a5be1c2f7610a750f5b659727772
rekkasa <alexandros.rekkas@gmail.com>
2025-06-12 15:59:26 +0300

    start defining settings for the outcome models

---
+++ R/Settings.R
@@ -0,0 +1,118 @@
+checkIfUsingSameSettings <- function(
+  settings,
+  targetClass,
+  nOutcomeIds,
+  settingsLabel = "label"
+) {
+
+  if (inherits(settings, targetClass)) {
+    message(glue::glue("Using same {settingsLabel} for all outcomes"))
+    return(TRUE)
+  } else if (is.list(settings)) {
+    if (length(settings) == nOutcomeIds) {
+      message(glue::glue("Using different {settingsLabel} for each outcome"))
+      return(FALSE)
+    } else {
+      stop(glue::glue("Length of {settingsLabel} list does not match number of outcomes"))
+    }
+  }
+
+
+  stop(glue::glue("{settingsLabel} must be a list or a '{targetClass}' object"))
+}
+
+createOutcomeModelSettings <- function(
+  databaseDetails,
+  targetId = 1,
+  outcomeIds,
+  populationSettings,
+  restrictPlpDataSettings,
+  covariateSettings,
+  featureEngineeringSettings,
+  sampleSettings,
+  splitSettings,
+  preprocessSettings,
+  modelSettings,
+  saveDirectory = "./OutcomeModels"
+) {
+
+  message("Generating outcome model settings ...")
+  nOutcomeIds <- length(outcomeIds)
+
+  meta <- list(
+    settings = list(
+      populationSettings,
+      restrictPlpDataSettings,
+      covariateSettings,
+      featureEngineeringSettings,
+      sampleSettings,
+      splitSettings,
+      preprocessSettings,
+      modelSettings
+    ),
+    targetClass = c(
+      "populationSettings",
+      "restrictPlpDataSettings",
+      "covariateSettings",
+      "featureEngineeringSettings",
+      "sampleSettings",
+      "splitSettings",
+      "preprocessSettings",
+      "modelSettings"
+    ),
+    settingsLabel = c(
+      "population settings",
+      "restrict plp data settings",
+      "covariate settings",
+      "feature engineering settings",
+      "sample settings",
+      "split settings",
+      "pre-process settings",
+      "model settings"
+    )
+  )
+
+  use_same_flags <- purrr::pmap_lgl(
+    .l = meta,
+    .f = checkIfUsingSameSettings,
+    nOutcomeIds = nOutcomeIds
+  )
+
+  names(use_same_flags) <- paste0(
+    "useSame",
+    c(
+      "PopulationSettings",
+      "RestrictPlpDataSettings",
+      "CovariateSettings",
+      "FeatureEngineeringSettings",
+      "SampleSettings",
+      "SplitSettings",
+      "PreprocessSettings",
+      "ModelSettings"
+    )
+  )
+
+  result <- c(
+    list(
+      databaseDetails = databaseDetails,
+      targetId = targetId,
+      outcomeIds = outcomeIds,
+      populationSettings = populationSettings,
+      restrictPlpDataSettings = restrictPlpDataSettings,
+      covariateSettings = covariateSettings,
+      featureEngineeringSettings = featureEngineeringSettings,
+      sampleSettings = sampleSettings,
+      splitSettings = splitSettings,
+      preprocessSettings = preprocessSettings,
+      modelSettings = modelSettings
+    ),
+    as.list(use_same_flags),
+    list(saveDirectory = saveDirectory)
+  )
+
+  class(result) <- "outcomeModelSettings"
+  message("Done")
+
+  result
+
+}
