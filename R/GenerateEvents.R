2577351ff71f5fa94798da248bf8f821a9e2fe74
rekkasa <alexandros.rekkas@gmail.com>
2025-06-30 15:43:39 +0300

    simulate outcomes faster

--- R/GenerateEvents.R
+++ R/GenerateEvents.R
@@ -1,96 +1,80 @@
 generateEventForSubject <- function(
-  rowId,
-  plpModel,
-  covariateData,
+  linearPredictor,
+  baselineSurvival,
   seed
 ) {
-
   if (missing(seed)) {
+
     seed <- sample(1:1e6, 1)
   }
 
-  times <- plpModel$model$baselineSurvival$time
-  baselineSurvival <- plpModel$model$baselineSurvival$surv
-  idx <- which(!duplicated(baselineSurvival))
+  nPatients <- nrow(linearPredictor)
+  linearPredictor <- linearPredictor |>
+    dplyr::mutate(
+      randomUniform = withr::with_seed(
+        seed,
+        runif(nPatients, 0, 1)
+      )
+    )
 
-  xx <- withr::with_seed(seed, runif(1, 0, 1))
-  thisRowId <- rowId
+  idx <- which(!duplicated(baselineSurvival$surv))
+  eventAtTime <- list()
+  survivedAtTime <- linearPredictor
+  for (i in seq_along(idx)) {
+    remainingAtTime <- survivedAtTime |>
+      dplyr::mutate(
+        survival = baselineSurvival$surv[idx[i]]^exp(linearPredictor),
+        event = survival < randomUniform,
+        time = baselineSurvival$time[idx[i]]
+      )
 
-  covariates <- covariateData$covariates |>
-    dplyr::collect() |>
-    dplyr::filter(rowId == thisRowId)
+    eventAtTime[[i]] <- remainingAtTime |>
+      dplyr::filter(event)
 
-  nonZeroCoef <- plpModel$model$coefficients |>
-    dplyr::filter(betas != 0) |>
-    dplyr::mutate(covariateIds = as.numeric(covariateIds))
-
-  predictions <- predictSurvivalOnSubject(
-    rowId = thisRowId,
-    plpModel = plpModel,
-    covariates = covariates,
-    nonZeroCoef = nonZeroCoef
-  )
-
-  if (xx < dplyr::last(predictions)) {
-    result <- Inf
-  } else {
-    k <- which(predictions < xx) |> min()
-    result <- times[idx[k]]
+    survivedAtTime <- remainingAtTime |>
+      dplyr::filter(!event)
   }
 
-  list(rowId = rowId, time = result)
-
+  data.table::rbindlist(eventAtTime, use.names = TRUE) |>
+    dplyr::as_tibble() |>
+    dplyr::bind_rows(survivedAtTime) |>
+    dplyr::mutate(time = ifelse(event, time, Inf)) |>
+    dplyr::arrange(rowId)
 }
 
-predictSurvivalOnSubject <- function(
-  rowId,
-  nonZeroCoef,
-  covariates
+
+computeLinearPredictor <- function(plpModel, covariateData) {
+  covs_dt <- data.table::as.data.table(
+    covariateData$covariates |> dplyr::collect()
+  )
+  dtCoef <- data.table::as.data.table(plpModel$model$coefficients)
+  dtCoef$covariateIds <- as.numeric(dtCoef$covariateIds)
+  nonZeroCoef <- dtCoef[dtCoef$betas != 0, ]
+
+  resDT <- covs_dt[
+    nonZeroCoef,
+    on = c("covariateId" = "covariateIds"),
+    nomatch = 0L
+  ][
+    , .(linearPredictor = sum(betas * covariateValue)),
+    by = rowId
+  ]
+
+  allRows <- data.table::data.table(rowId = unique(covs_dt$rowId))
+  outDT <- base::merge(allRows, resDT, by = "rowId", all.x = TRUE)
+  outDT$linearPredictor[is.na(outDT$linearPredictor)] <- 0
+
+  dplyr::as_tibble(outDT)
+}
+
+generateNewExposureTable <- function(
+  connectionDetails,
+  exposureDatabaseSchema,
+  exposureTable,
+  resultDatabaseSchema,
+  resultTable,
+  eventTimes
 ) {
 
-
-  times <- plpModel$model$baselineSurvival$time
-  baselineSurvival <- plpModel$model$baselineSurvival$surv
-  idx <- which(!duplicated(baselineSurvival))
-
-  .predict <- function(
-    id,
-    covariates,
-    times,
-    baselineSurvival,
-    nonZeroCoef
-  ) {
-
-    s0 <- baselineSurvival[id]
-    survival <- function(x, s0) {
-      s0^exp(x)
-    }
-
-    dd <- covariates |>
-      dplyr::left_join(nonZeroCoef, by = c("covariateId" = "covariateIds")) |>
-      dplyr::filter(!is.na(betas))
-    if (nrow(dd) == 0) {
-      NULL
-    } else {
-
-      dd |>
-      dplyr::mutate(value = covariateValue * betas) |>
-      dplyr::pull(value) |>
-      sum() |>
-      survival(s0 = s0)
-
-    }
-
-  }
-
-  purrr::map_dbl(
-    .x = idx,
-    .f = .predict,
-    times = times,
-    baselineSurvival = baselineSurvival,
-    covariates = covariates,
-    nonZeroCoef = nonZeroCoef
-  )
-
+  TRUE
 }
-
